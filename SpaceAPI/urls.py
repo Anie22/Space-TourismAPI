f7daa967e2f482cc758f84cb2fa0ebadfea3a095
Aniebiet Francis <francisaniebiet678@gmail.com>
2023-10-03 20:07:00 +0100

    first commit

--- SpaceAPI/urls.py
+++ SpaceAPI/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from SpaceAPI.views import *
 
 urlpatterns = [
-    path('destination/', DestinationViewSet.as_view({'get': 'list'}), name='destination'),
+    path('', DestinationViewSet.as_view({'get': 'list'}), name='destination'),
     path('crew/', CrewViewSet.as_view({'get': 'list'}), name='crew'),
     path('technology/', TechnologyViewSet.as_view({'get': 'list'}), name='technologie'),
 ]
